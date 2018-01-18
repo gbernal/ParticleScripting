5917bdf649da38d5ccd0ad52f47de4d1e9be020b
Guillermo Bernal <gbernal@mit.edu>
2018-01-18 13:52:47 -0500

    Added skin mesh coed

---
+++ Assets/SKM_Vertices.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SKM_Vertices : MonoBehaviour {
+
+    Mesh mesh;
+    SkinnedMeshRenderer skin;
+
+    int vertexCount;
+    Vector3[] vertices;
+    Vector3[] normals;
+    public System.Action<SKM_Vertices> OnResultsReady;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        skin = GetComponent<SkinnedMeshRenderer>();
+        mesh = skin.sharedMesh;
+        skin.BakeMesh(mesh);
+        vertexCount = mesh.vertexCount;
+        vertices = new Vector3[vertexCount];
+        Debug.Log(vertexCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        vertices = new Vector3[mesh.vertices.Length]; mesh.vertices.CopyTo(vertices, 0);
+
+
+    }
+}
